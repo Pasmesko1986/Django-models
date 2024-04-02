c591b5c2acbdacff90499e99101d61c439af0267
Pasmesko1986 <pasmesko1986@gmail.com>
2024-04-02 17:55:38 -0300

    .

--- ejemplo_clase/marvel/e_commerce/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-# Create your views here.
-def hola_mundo(request):    
-    datos =  {"mensaje": "hola_mundo", "curso": "python-django"}
-    return JsonResponse(datos)
-
-# 2:44:22
